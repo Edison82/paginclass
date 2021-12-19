05dc0a2f205eb60426d40a89d5322cb3fe7dfd18
Edison82 <ediflow82@gmail.com>
2021-12-19 19:12:20 +0100

    Se termina toda la pagina

---
+++ EjecutablePagina.js
@@ -0,0 +1,14 @@
+
+function siguiente (){
+
+    let tipodesuscripcion, nombres, email, mediodepago
+
+    tipodesuscripcion = document.getElementById('tipodesuscripcion').value
+    nombres = document.getElementById('nombres').value
+    email = document.getElementById('email').value
+    mediodepago = document.getElementById('mediodepago').value
+
+    alert( "Confirmas esta información " + tipodesuscripcion + "  " + nombres + "  " + email + "  " + mediodepago + "  " )
+
+    
+}
